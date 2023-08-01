4379b79d985c03d47f7d7b4a0103d194edf199c1
Shako Kushashvili <60470121+ShakNorris@users.noreply.github.com>
2023-07-31 21:19:37 -0400

    Add files via upload

---
+++ src/spotify-to-mp3.py
@@ -0,0 +1,47 @@
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+print(client_secret)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri="http://localhost:8888/callback",
+                                               scope="user-library-read"))
+
+
+def get_artist_id(artistName: str):
+    """This function retrieves artist's ID"""
+    searchID = sp.search(q='artist:' + artistName, type='artist')
+    ID = searchID['artists']['items'][0]['id']
+    return ID
+
+
+def get_album(artist: str, album: str):
+    """This function retrieves songs from an album"""
+    ID = get_artist_id(artist)
+    results = sp.artist_albums(ID, album_type='album')
+    albums = results['items']
+    for x in albums:
+        if x['name'] == album:
+            tracks = sp.album_tracks(x['id'])
+            for track in tracks['items']:
+                print(json.dumps(track))
+
+
+def get_song(artist: str, song: str):
+    """This function retrieves a song"""
+    q = f"artist:{artist} track:{song}"
+    results = sp.search(q=q, type='track')
+    tracks = results['tracks']['items']
+    print(tracks)
+    for track in tracks:
+        print(track['artists'][0]['name'] + ' - ' + track['name'])
+
+
+get_album('Logic', 'No Pressure')
